666e57e772cc31fee3a93591f8101d67658a9fb7
Alexey Efiomv <lex.efimov@gmail.com>
2016-04-24 19:15:47 +0500

    Запись объекта NSData в файл

--- ImageFetch/main.m
+++ ImageFetch/main.m
@@ -10,8 +10,27 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        NSURL *url = [NSURL URLWithString:@"http://www.google.com/images/logos/ps_logo2.png"];
+        NSURLRequest *recuest = [NSURLRequest requestWithURL:url];
+        NSError *error = nil;
+        NSData *data = [NSURLConnection sendSynchronousRequest:recuest
+                                             returningResponse:NULL
+                                                         error:&error];
+        if (!data) {
+            NSLog(@"fetch failed: %@", [error localizedDescription]);
+            return 1;
+        }
+        
+        NSLog(@"The file is %lu bytes", [data length]);
+        
+        BOOL writen = [data writeToFile:@"/tmp/google.png"
+                                options:0
+                                  error:&error];
+        if (!writen) {
+            NSLog(@"write failed: %@", [error localizedDescription]);
+            return 1;
+        }
+        NSLog(@"Success!");
     }
     return 0;
 }
